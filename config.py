8b6e57b91aad785def1547bf55a07b8994657e70
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-25 22:09:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5679702303:AAHt0T6CRBAi6H5XfRufu2A5L4TCU99hqq8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5817548900:AAGLrVfoYCbsIkReRzExhLsFcOAO9hnh8P0")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "16246834"))
@@ -12,30 +12,30 @@ APP_ID = int(os.environ.get("APP_ID", "16246834"))
 API_HASH = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001636429528"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001906228378"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", "7423630"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1908660708"))
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "postgres://azbxclgr:YADtY7HIPvuaHWXMhZ_MXXksMXJCkegm@queenie.db.elephantsql.com/azbxclgr")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001875335696"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001709247243"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nJoin group dan Channel gua dulu ya bang kalaw mau dapat video free.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nUntuk mendapatkan video anda harus subscribe channel DRAKOR TV terlebih dahulu kemudian tekan mulai kembali pada bot.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "5591632852").split()):
+    for x in (os.environ.get("ADMINS", "1908660708").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Join Grup dan Channelnya dulu ya\n\nKalaw mau nggak ribet join sana sini order VIP ke admin ya</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Untuk mendapatkan video anda harus subscribe channel Drakor TV terlebih dahulu, kemudian tekan mulai kembali pada bot</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -49,8 +49,8 @@ if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
 else:
     DISABLE_CHANNEL_BUTTON = False
 
-ADMINS.append(7423630)
-ADMINS.append(5591632852)
+ADMINS.append(1908660708)
+ADMINS.append(0)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
