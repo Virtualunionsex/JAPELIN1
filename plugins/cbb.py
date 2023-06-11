9bf13fba796f053eb0cf162b9c99098f9ff3f12f
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-06-12 01:11:12 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Dionprastt/protectconten'>Click here</a>\n○ Channel : @File_Sharing_Support\n○ Owner Repo: @ItsMeYons</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/nakama_asl'>Click here</a>\n○ Channel : @executive02\n○ Owner Repo: @azure_id</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
